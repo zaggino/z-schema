5380b651b8565dc3325d7fc2740c9937d7e1961f
Martin Zagora <zaggino@gmail.com>
2014-08-21 15:13:57 +1000

    Fix testling tests (hopefully)

--- test/Runner.js
+++ test/Runner.js
@@ -1,4 +1,5 @@
+/*global jasmine, jasmineReporters*/
 var env = jasmine.getEnv();
-var reporter = new jasmine.TapReporter();
+var reporter = new jasmineReporters.TapReporter();
 env.addReporter(reporter);
 env.execute();
