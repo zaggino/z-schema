c80f4b5c967b1e6e0bc3bdb87ab1c838a4844bc5
simon-p-r <simon.ricaldone@circabs.com>
2015-01-27 21:41:54 +0000

    Fixed report object object error

--- src/Report.js
+++ src/Report.js
@@ -95,11 +95,13 @@ Report.prototype.addError = function (errorCode, params, subReports, schemaDescr
 
     params = params || [];
 
-    var idx = params.length,
-        errorMessage = Errors[errorCode];
-    while (idx--) {
-        errorMessage = errorMessage.replace("{" + idx + "}", params[idx]);
-    }
+	var m = "";
+	var idx = params.length,
+		errorMessage = Errors[errorCode];
+	while (idx--) {
+		m = (typeof params[idx] === "object" ? JSON.stringify(params[idx]) : params[idx]);
+		errorMessage = errorMessage.replace("{" + idx + "}", (m ? m : "null"));
+	}
 
     var err = {
         code: errorCode,
