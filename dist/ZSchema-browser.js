64d55e37de5b2fd2b0ca089d7e47f2a2bc666a7f
Martin Zagora <zaggino@gmail.com>
2015-06-15 07:37:04 +0200

    Version 3.12.0

--- dist/ZSchema-browser.js
+++ dist/ZSchema-browser.js
@@ -122,7 +122,7 @@ process.chdir = function (dir) {
 
     'use strict';
 
-    validator = { version: '3.36.0' };
+    validator = { version: '3.39.0' };
 
     var emailUser = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e])|(\\[\x01-\x09\x0b\x0c\x0d-\x7f])))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))$/i;
 
@@ -172,7 +172,9 @@ process.chdir = function (dir) {
       'fr-FR': /^(\+?33|0)[67]\d{8}$/,
       'pt-PT': /^(\+351)?9[1236]\d{7}$/,
       'el-GR': /^(\+30)?((2\d{9})|(69\d{8}))$/,
-      'en-GB': /^(\+?44|0)7\d{9}$/
+      'en-GB': /^(\+?44|0)7\d{9}$/,
+      'en-US': /^(\+?1)?[2-9]\d{2}[2-9](?!11)\d{6}$/,
+      'en-ZM': /^(\+26)?09[567]\d{7}$/
     };
 
     validator.extend = function (name, fn) {
@@ -247,7 +249,7 @@ process.chdir = function (dir) {
     var default_email_options = {
         allow_display_name: false,
         allow_utf8_local_part: true,
-        require_tld: false
+        require_tld: true
     };
 
     validator.isEmail = function (str, options) {
@@ -466,12 +468,14 @@ process.chdir = function (dir) {
         return str === str.toUpperCase();
     };
 
-    validator.isInt = function (str) {
-        return int.test(str);
+    validator.isInt = function (str, options) {
+        options = options || {};
+        return int.test(str) && (!options.hasOwnProperty('min') || str >= options.min) && (!options.hasOwnProperty('max') || str <= options.max);
     };
 
-    validator.isFloat = function (str) {
-        return str !== '' && float.test(str);
+    validator.isFloat = function (str, options) {
+        options = options || {};
+        return str !== '' && float.test(str) && (!options.hasOwnProperty('min') || str >= options.min) && (!options.hasOwnProperty('max') || str <= options.max);
     };
 
     validator.isDivisibleBy = function (str, num) {
@@ -3263,6 +3267,9 @@ ZSchema.setSchemaReader = function (schemaReader) {
 };
 ZSchema.registerFormat = function (formatName, validatorFunction) {
     FormatValidators[formatName] = validatorFunction;
+};
+ZSchema.getRegisteredFormats = function () {
+    return Object.keys(FormatValidators);
 };
 ZSchema.getDefaultOptions = function () {
     return Utils.cloneDeep(defaultOptions);
