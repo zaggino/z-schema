a1ed621eb67b79dcf8abf85d7164918abe7ce389
Sergey Shandar <sergey.shandar@gmail.com>
2018-08-27 13:04:24 -0700

    Unique symbols

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
-export declare const schemaSymbol: Symbol
+export declare const schemaSymbol: unique Symbol
 
-export declare const jsonSymbol: Symbol
+export declare const jsonSymbol: unique Symbol
