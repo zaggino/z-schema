924ef0f94fef0c00342344f9b7f37002dc9572d4
Martin Zagora <zaggino@gmail.com>
2026-02-25 06:51:58 +0100

    feat: final draft-07 commit

--- src/json-schema-versions.ts
+++ src/json-schema-versions.ts
@@ -4,7 +4,7 @@ import type { JsonSchemaCommon, ZSchemaInternalProperties } from './json-schema.
 // TODO: currently unsupported '2019-09', '2020-12'
 export type JsonSchemaVersion = 'draft-04' | 'draft-06' | 'draft-07';
 
-export const CURRENT_DEFAULT_SCHEMA_VERSION: JsonSchemaVersion = 'draft-06';
+export const CURRENT_DEFAULT_SCHEMA_VERSION: JsonSchemaVersion = 'draft-07';
 
 export const VERSION_SCHEMA_URL_MAPPING: Record<JsonSchemaVersion, string> = {
   'draft-04': 'http://json-schema.org/draft-04/schema#',
