dc8fef331e06221bd33fe56450a59b0073d17441
Martin Zagora <zaggino@gmail.com>
2026-03-20 11:11:24 +0100

    chore: minor fixes

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,6 @@
 const lintStagedConfig = {
-  '*.(ts|tsx|mts|cts|js|jsx|mjs|cjs)': ['oxlint --fix'],
-  '*': ['oxfmt --no-error-on-unmatched-pattern --write'],
+  '*.(ts|tsx|mts|cts|js|jsx|mjs|cjs)': ['oxlint --fix', 'oxfmt --write'],
+  '!(*.(ts|tsx|mts|cts|js|jsx|mjs|cjs))': ['oxfmt --no-error-on-unmatched-pattern --write'],
 };
 
 export default lintStagedConfig;
